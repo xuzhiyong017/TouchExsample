8abd6ec2ff6a936e96a21d6c0ae9e7eb363e9634
xuzhiyong <18971269648@163.com>
2021-08-19 10:10:41 +0800

    水滴气泡

--- app/src/main/java/com/example/testexample/DrawTestView.kt
+++ app/src/main/java/com/example/testexample/DrawTestView.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.*
 import android.os.Build
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import androidx.annotation.RequiresApi
 import px
@@ -22,34 +21,47 @@ class DrawTestView (context: Context, attrs: AttributeSet?) : View(context,attrs
         textSize = 80f.px
         textAlign = Paint.Align.CENTER
     }
-    val RADIUS = 100f.px
-    val OPEN_ANGLE = 120f
-    val DASH_WIDTH = 2f.px
-    val DASH_LENGTH = 10f.px
-    val dash = Path()
-    lateinit var pathEffect: PathDashPathEffect
-    val bounds: RectF = RectF()
-    val fontMetrics = Paint.FontMetrics()
+
+    val bounds = RectF()
+    val path = Path()
+    val wide = 17f.px.toInt()
+
     init {
-        paint.strokeWidth = 3f.px
-        paint.style = Paint.Style.STROKE
-        pathEffect = PathDashPathEffect(dash,50f,0f, PathDashPathEffect.Style.ROTATE)
+        paint.strokeWidth = 1f.px
+        paint.style = Paint.Style.FILL
+
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(wide * 3,wide * 3)
+        bounds.set(0f,wide / 2f,wide * 2f,wide * 2f + wide / 2f)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        bounds.set(0f,0f,RADIUS,RADIUS)
-        dash.reset()
-        dash.arcTo(bounds, 60.0f, 240.0f)
-        dash.quadTo(RADIUS /2,
-            (RADIUS /2 - (sqrt(3.0) / 2.0 * (RADIUS / 3))).toFloat(),
-            RADIUS - RADIUS /3/3,RADIUS / 2);
-        dash.close()
+        path.reset()
+        path.arcTo(bounds, 60.0f, 240.0f)
+        path.lineTo(width - wide / 3f,height / 2f)
+//        path.quadTo(wide * 3f / 2,
+//            (width / 2 - ((sqrt(3.0) / 2.0 )* wide)).toFloat(),
+//            width - wide / 3f,height / 2f);
+        path.close()
 
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onDraw(canvas: Canvas) {
-        canvas.drawPath(dash,paint)
+        canvas.drawPath(path,paint)
+//        canvas.drawCircle(wide * 3f / 2,(width / 2 - ((sqrt(3.0) / 2.0 )* wide)).toFloat(),5f,Paint().apply {
+//            setColor(Color.RED)
+//        })
+
+//        canvas.drawCircle(width - wide / 3f,height / 2f,1f,paint)
+//        canvas.drawCircle(bounds.centerX(),bounds.centerY(),1f,paint)
+//        canvas.drawRect(bounds,Paint().apply {
+//            color = Color.BLUE
+//            style = Paint.Style.STROKE
+//        })
     }
 }
